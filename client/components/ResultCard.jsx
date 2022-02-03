166b86f396d44bd995c68f4fd98fffbd42a1a12f
Julia Bakerink <juliabakerink@Julias-MacBook-Pro.local>
2022-02-02 17:31:40 -0800

    attempts at testing, one test complete on resultcard - JIWON/JULIA

--- client/components/ResultCard.jsx
+++ client/components/ResultCard.jsx
@@ -8,11 +8,11 @@ const ResultCard = (props) => {
       <div className="business">
         <img className="businessImg"src={props.image}></img>
         <div className="businessDetails">
-          <p className="distance">{props.distance}</p>
-          <p><a className="name" href={props.url}>{props.name}</a></p>
-          <p><span className="price">{props.price}</span><span>&#8226;</span><span className="rating">Rating: {props.rating}</span></p>
-          <p className="Address">{props.address}</p>
-          <p className="phone">{props.phone}</p>
+          <p data-testid="distance-element" className="distance">{props.distance}</p>
+          <p> <a className="name" href={props.url}>{props.name}</a></p>
+          <p><span data-testid="price-element" className="price">{props.price}</span><span>&#8226;</span><span data-testid="rating-element" className="rating">Rating: {props.rating}</span></p>
+          <p data-testid="address-element" className="Address">{props.address}</p>
+          <p data-testid="phone-element" className="phone">{props.phone}</p>
           <button className="add-comment">Show Comments</button>
         </div>
       </div>
@@ -20,7 +20,7 @@ const ResultCard = (props) => {
         <button className="addFav" aria-label="Add to favorites" onClick={() => props.addFav(props.result)}>Favorite</button>
         <button className="comment" aria-label="Add comment" onClick={props.addComment}>Comment</button>
         <CopyToClipboard text={props.url}>
-          <button className="share" aria-label="Share link" onClick={(e) => {e.target.innerText = 'Copied!'; setTimeout(() => {e.target.innerText = 'Share'}, 1000) }}>Share</button>
+          <button data-testid="buttonCopy" className="share" aria-label="Share link" onClick={(e) => {e.target.innerText = 'Copied!'; setTimeout(() => {e.target.innerText = 'Share'}, 1000) }}>Share</button>
         </CopyToClipboard>
       </div>
     </article>
